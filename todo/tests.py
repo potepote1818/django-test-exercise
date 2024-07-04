ef00ccc39073df24715da11de592e891ecd646a0
1f10230002 <s1f102300021@iniad.org>
2024-07-04 14:13:42 +0900

    コードの整形4

--- todo/tests.py
+++ todo/tests.py
@@ -3,8 +3,8 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-# Create your tests here.
 
+# Create your tests here.
 class SampleTestCase(TestCase):
     def test_sample1(self):
         self.assertEqual(1+2, 3)
@@ -59,9 +59,10 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(len(response.context['tasks']), 0)
+
     def test_index_post(self):
         client = Client()
-        data = {'title':'Test Task', 'due_at':'2024-06-30 23:59:59'}
+        data = {'title': 'Test Task', 'due_at': '2024-06-30 23:59:59'}
         response = client.post('/', data) 
 
         self.assertEqual(response.status_code, 200)
